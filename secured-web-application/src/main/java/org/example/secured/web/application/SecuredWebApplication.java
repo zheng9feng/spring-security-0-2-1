724fb56046e511b078a058ac1ace738c3791d36e
m0v1 <mail@mail>
2025-11-06 15:46:58 +0800

    build: initialize project with Maven setup and Spring Boot application

---
+++ secured-web-application/src/main/java/org/example/secured/web/application/SecuredWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.secured.web.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredWebApplication.class, args);
+	}
+
+}
