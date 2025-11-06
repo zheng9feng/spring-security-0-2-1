59757ca6e7713096e74c0565a73b616186a0142b
m0v1 <mail@mail>
2025-11-06 15:29:52 +0800

    build: initialize project with Maven setup and Spring Boot application

---
+++ unsecured-web-application/src/main/java/org/example/unsecured/web/application/UnsecuredWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.unsecured.web.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsecuredWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsecuredWebApplication.class, args);
+	}
+
+}
---
+++ unsecured-web-application/src/test/java/org/example/unsecured/web/application/UnsecuredWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.unsecured.web.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsecuredWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
