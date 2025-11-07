16c7ce4b1dd36a7410ee4c07b94de906fa121add
m0v1 <mail@mail>
2025-11-07 09:09:31 +0800

    build: initialize project with Maven setup and Spring Boot application

---
+++ hello-spring-security/src/main/java/org/example/hello/spring/security/HelloSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.hello.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ hello-spring-security/src/test/java/org/example/hello/spring/security/HelloSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hello.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
