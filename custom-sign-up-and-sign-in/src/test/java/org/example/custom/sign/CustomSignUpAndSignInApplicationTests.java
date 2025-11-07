901637be2760588ca50596d45ec1c0eaf329d750
m0v1 <mail@mail>
2025-11-07 14:17:16 +0800

    build: initialize project with Maven setup and Spring Boot application

---
+++ custom-sign-up-and-sign-in/src/test/java/org/example/custom/sign/CustomSignUpAndSignInApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.custom.sign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSignUpAndSignInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
