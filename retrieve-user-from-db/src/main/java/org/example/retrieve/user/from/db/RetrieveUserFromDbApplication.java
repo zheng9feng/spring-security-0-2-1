3fce41be2516bf5873b6f643b1f8ed307319cf0a
m0v1 <mail@mail>
2025-11-22 02:12:07 +0800

    feat: initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ retrieve-user-from-db/src/main/java/org/example/retrieve/user/from/db/RetrieveUserFromDbApplication.java
@@ -0,0 +1,13 @@
+package org.example.retrieve.user.from.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrieveUserFromDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrieveUserFromDbApplication.class, args);
+    }
+
+}
---
+++ retrieve-user-from-db/src/test/java/org/example/retrieve/user/from/db/RetrieveUserFromDbApplicationTests.java
@@ -0,0 +1,12 @@
+package org.example.retrieve.user.from.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrieveUserFromDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
