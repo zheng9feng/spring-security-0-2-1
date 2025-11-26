4956f8e9747b4e0ead41a80c9043e36dfcb73704
m0v1 <mail@mail>
2025-11-26 15:47:48 +0800

    fix: update doGet method signature in HelloServlet to include ServletException

--- filter-sample-in-servlet/src/main/java/org/example/filter/HelloServlet.java
+++ filter-sample-in-servlet/src/main/java/org/example/filter/HelloServlet.java
@@ -1,5 +1,6 @@
 package org.example.filter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,7 +10,7 @@ import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter pw = resp.getWriter();
         pw.write("<h1>Hello, world! This is from HelloServlet!</h1>");
