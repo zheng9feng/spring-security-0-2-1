bcd23203c10517455fc730fc3390311a4e4f8063
m0v1 <mail@mail>
2025-11-26 15:29:46 +0800

    feat: add HelloServlet to handle /hello requests

---
+++ filter-sample-in-servlet/src/main/java/org/example/filter/HelloServlet.java
@@ -0,0 +1,19 @@
+package org.example.filter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+        pw.write("<h1>Hello, world! This is from HelloServlet!</h1>");
+        pw.flush();
+    }
+}
